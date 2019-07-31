d5fa1b4dd37ce22e1744f5a03d52f4891f6c0c70
veshen <13120860906@163.com>
2019-08-01 00:30:20 +0800

    我得睡了 这是一个向晚安妥协的版本

--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { renderRoutes } from 'react-router-config'
-import store from './store/store'
+// import { Provider } from 'react-redux'
+// import { BrowserRouter as Router } from 'react-router-dom'
+// import { renderRoutes } from 'react-router-config'
+
+import MainRouter from './modules/MainRouter';
+
+// import store from './store/store'
 // import routes from './routes-config.js'
-import Home from './pages/home/index.connect';
-import Login from './pages/login';
+// import Home from './pages/home/index.connect';
+// import Login from './pages/login';
 // import NotFound from './404';
-interface TypeOfRouterItem {
-    path : string
-    exact? : boolean
-    component : any
-}
-const routes:Array<TypeOfRouterItem> = [
-    {   path: '/',
-        exact: true,
-        component: Home,
-    },
-    {
-        path: '/login',
-        component: Login,
-    },
-    // {
-    //     path: '/user',
-    //     component: User,
-    // },
-    // {
-    //     path: '*',
-    //     component: NotFound
-    // }
-];
-ReactDOM.render(
-  <Provider store={store}>
-    <Router>{renderRoutes(routes)}</Router>
-  </Provider>,
-  document.getElementById('root')
-)
+// interface TypeOfRouterItem {
+//     path : string
+//     exact? : boolean
+//     component : any
+// }
+// const routes:Array<TypeOfRouterItem> = [
+//     {   path: '/',
+//         exact: true,
+//         component: Home,
+//     },
+//     {
+//         path: '/login',
+//         component: Login,
+//     },
+//     // {
+//     //     path: '/user',
+//     //     component: User,
+//     // },
+//     // {
+//     //     path: '*',
+//     //     component: NotFound
+//     // }
+// ];
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <Router>{renderRoutes(routes)}</Router>
+//   </Provider>,
+//   document.getElementById('root')
+// )
+
+// eslint-disable-next-line no-undef
+ReactDOM.render(<MainRouter />, document.getElementById('root'));
